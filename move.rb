47c79378c7532e579ef7ba5817f9e5bf42e70405
Ashley McKenzie <ash@ashmckenzie.org>
2010-01-29 11:08:01 +1100

    - initial commit

---
+++ move.rb
@@ -0,0 +1,43 @@
+#!/usr/bin/env ruby
+
+# Author: Ash McKenzie (ash@ashmckenzie.org)
+#
+# Simple Ruby script to move files from one location to another based
+# on a defined set of rules in config.yml.
+#
+# Look for NOTE: entries for things to change.
+
+require 'rubygems'
+require 'yaml'
+require 'fileutils'
+
+config = YAML.load_file('config.yml')
+
+src_dir = config['look']['dir']
+dir = Dir.new(src_dir)
+regex = Regexp.new(config['look']['regex'], Regexp::IGNORECASE)
+
+files = []
+
+puts "- Looking for files to move in #{src_dir}"
+
+dir.each do |entry|
+  if regex.match(entry)
+    files << entry
+  end
+end
+
+dir.close
+
+files.each do |file|
+  config['mappings'].each do |mapping|
+    dir = mapping['dir']
+    regex = Regexp.new(mapping['regex'], Regexp::IGNORECASE)
+    if m = r.match(file)
+      dest_dir = "#{dir}/Season #{m[1].to_i}/"    # NOTE: Change this to suit
+      puts "- Attempting to move #{file} to #{dest_dir}"
+      Dir.mkdir(dest_dir) unless File.directory?(dest_dir)
+      FileUtils.mv("#{src_dir}/#{file}", "#{dest_dir}/#{file}")
+    end
+  end
+end
